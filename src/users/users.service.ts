cb4ef743336891456473ab86fe71c8f1c9988a3c
minhbear <hvmnhatminh@gmail.com>
2023-02-28 09:53:11 +0700

    refactor(users.service): update createAccountWithTransaction and remove comment

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -23,21 +23,11 @@ export class UsersService extends BaseService<
     super(MessageName.USER, accountRepository);
   }
 
-  // async createAccountWithTransaction(
-  //   createUserDto: CreateUserDto,
-  //   manager: EntityManager,
-  // ): Promise<AccountEntity> {
-  //   const account = await manager.save(AccountEntity, createUserDto);
-  //   return account;
-  // }
-
-  // create a createAccountWithTransaction generic function
   async createAccountWithTransaction<T>(
     createUserDto: T,
     manager: EntityManager
   ) {
-    const account = await manager.save(AccountEntity, createUserDto);
-    return account;
+    return await manager.save(AccountEntity, createUserDto);
   }
 
   async createAccount<T>(createUserDto: T): Promise<AccountEntity> {
