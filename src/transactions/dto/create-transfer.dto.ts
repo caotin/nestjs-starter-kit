cd2ec120e7f0b3e7610a114babbae5de1fa1b78f
Nguyễn Thành Thiện <thanhthien24102002@gmail.com>
2023-03-06 15:51:01 +0700

    fix: validation transfer api

--- src/transactions/dto/create-transfer.dto.ts
+++ src/transactions/dto/create-transfer.dto.ts
@@ -1,15 +1,26 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber, IsString } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsNumberString,
+  IsString,
+  Matches,
+  Min,
+} from 'class-validator';
 
 export class CreateTransferDto {
   from?: number;
 
   @ApiProperty()
   @IsNumber()
+  @IsNotEmpty()
   to: number;
 
   @ApiProperty()
   @IsString()
+  @IsNotEmpty()
+  @IsNumberString()
+  @Min(0)
   amount: string;
 
   @ApiProperty()
